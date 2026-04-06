24f25c9a53b127890da57e3a5e8faf75ba0b9ed7
scrom-2511 <formain337@gmail.com>
2026-04-07 00:37:16 +0530

    add: build_queue.rs file

---
+++ src/services/build_queue.rs
@@ -0,0 +1,46 @@
+use lapin::{
+    Channel, Connection, Queue,
+    types::{AMQPValue, FieldTable, LongString, ShortString},
+};
+
+use crate::{app_errors::AppError, services::lapin::Lapin};
+
+struct BuildQueue<'a> {
+    connection: &'a Connection,
+    channel: Channel,
+    queue: Queue,
+}
+
+impl<'a> BuildQueue<'a> {
+    pub async fn new(lapin_conn: &'a Lapin) -> Result<Self, AppError> {
+        let connection = lapin_conn.get_connection().await;
+
+        let channel = connection
+            .create_channel()
+            .await
+            .map_err(|e| AppError::ChannelError(e.to_string()))?;
+
+        let mut queue_args = FieldTable::default();
+        queue_args.insert(
+            ShortString::from("x-queue-type"),
+            AMQPValue::LongString(LongString::from("quorum")),
+        );
+
+        let queue = channel
+            .queue_declare(
+                ShortString::from("build_queue"),
+                Default::default(),
+                queue_args,
+            )
+            .await
+            .map_err(|e| AppError::QueueError(e.to_string()))?;
+
+        Ok(Self {
+            connection,
+            channel,
+            queue,
+        })
+    }
+
+    pub async fn publish(&self, message: &str) -> Result<(), AppError> {}
+}
