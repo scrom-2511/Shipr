d0d0fbe185b8c52bb6e5061e3ee05f4d1dbd6509
scrom-2511 <formain337@gmail.com>
2026-04-08 00:32:53 +0530

    add: new fn in build_queue.rs to create a new object

---
+++ src/services/lapin/build_queue.rs
@@ -0,0 +1,46 @@
+use futures::stream::StreamExt;
+use lapin::{
+    Channel, Connection, Queue,
+    options::BasicAckOptions,
+    types::{AMQPValue, FieldTable, LongString, ShortString},
+};
+
+use crate::{app_errors::AppError, app_types::DeployDetails, services::lapin::lapin::Lapin};
+
+struct BuildQueue<'a> {
+    connection: &'a Connection,
+    channel: Channel,
+    queue: Queue,
+}
+
+impl<'a> BuildQueue<'a> {
+    pub async fn new(lapin_conn: &'a Lapin) -> Result<Self, AppError> {
+        let connection = lapin_conn.get_connection().await;
+
+        let channel = connection
+            .create_channel()
+            .await
+            .map_err(|e| AppError::ChannelError(e.to_string()))?;
+
+        let mut queue_args = FieldTable::default();
+        queue_args.insert(
+            ShortString::from("x-queue-type"),
+            AMQPValue::LongString(LongString::from("quorum")),
+        );
+
+        let queue = channel
+            .queue_declare(
+                ShortString::from("build_queue"),
+                Default::default(),
+                queue_args,
+            )
+            .await
+            .map_err(|e| AppError::QueueError(e.to_string()))?;
+
+        Ok(Self {
+            connection,
+            channel,
+            queue,
+        })
+    }
+}
