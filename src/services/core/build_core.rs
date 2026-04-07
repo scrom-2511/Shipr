045a6d2512986ba34da5293e0b6128053d51585d
scrom-2511 <formain337@gmail.com>
2026-04-08 00:37:22 +0530

    add: run_cmds fn in build_core.rs to run the install and build cmds

--- src/services/core/build_core.rs
+++ src/services/core/build_core.rs
@@ -8,4 +8,32 @@ impl BuildCore {
     pub fn new() -> Self {
         Self {}
     }
+
+    pub async fn run_cmds(cmds: &Vec<String>) -> Result<(), AppError> {
+        if cmds.len() == 0 {
+            return Ok(());
+        }
+
+        for cmd in cmds {
+            let cmd_parts: Vec<&str> = cmd.split_whitespace().collect();
+
+            let initial_part = cmd_parts
+                .get(0)
+                .ok_or_else(|| AppError::CmdFailed("Empty command".into()))?;
+
+            let output = Command::new(initial_part)
+                .args(&cmd_parts[1..])
+                .output()
+                .await
+                .map_err(|e| AppError::CmdFailed(e.to_string()))?;
+
+            if !output.status.success() {
+                return Err(AppError::CmdFailed(
+                    String::from_utf8_lossy(&output.stderr).to_string(),
+                ));
+            }
+        }
+
+        Ok(())
+    }
 }
