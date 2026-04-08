d9a4ce217cfdc538dad6f6dd13c48b183aa089c8
scrom-2511 <formain337@gmail.com>
2026-04-09 01:23:04 +0530

    fix: start fn

--- src/services/firecracker/firecracker.rs
+++ src/services/firecracker/firecracker.rs
@@ -1,39 +1,61 @@
-use std::fs;
-
 use tokio::process::Command;
 
 use crate::app_errors::AppError;
 
-struct Firecracker;
+pub struct Firecracker;
 
 impl Firecracker {
     pub fn new() -> Self {
         Self
     }
 
+    async fn run_cmds(cmds: &Vec<String>) -> Result<(), AppError> {
+        if cmds.len() == 0 {
+            return Ok(());
+        }
+
+        for cmd in cmds {
+            let cmd_parts: Vec<&str> = cmd.split_whitespace().collect();
+
+            print!("{:?}", cmd_parts);
+
+            let initial_part = cmd_parts
+                .get(0)
+                .ok_or_else(|| AppError::CmdFailed("Empty command".into()))?;
+
+            let output = Command::new(initial_part)
+                .args(&cmd_parts[1..])
+                .output()
+                .await
+                .map_err(|e| AppError::CmdFailed(e.to_string()))?;
+
+            if !output.status.success() {
+                return Err(AppError::CmdFailed(
+                    String::from_utf8_lossy(&output.stderr).to_string(),
+                ));
+            }
+        }
+
+        Ok(())
+    }
+
     pub async fn start() -> Result<(), AppError> {
         let API_SOCKET = "/tmp/firecracker.socket";
 
-        fs::remove_file(API_SOCKET);
+        let cleanup_cmd = format!("sudo rm -f {}", API_SOCKET);
 
-        let firecracker_run_cmd = format!(
-            "sudo ./firecracker-main --api-sock {} --enable-pci",
-            API_SOCKET
-        );
+        let firecracker_path = "/home/scrom/firecracker-main";
 
-        let firecracker_run_cmd_parts: Vec<&str> = firecracker_run_cmd.split_whitespace().collect();
+        Self::run_cmds(&vec![cleanup_cmd]).await?;
 
-        let output = Command::new(&firecracker_run_cmd_parts[0])
-            .args(&firecracker_run_cmd_parts[1..])
-            .output()
-            .await
-            .map_err(|e| AppError::StartingFirecrackerFailed(e.to_string()))?;
-
-        if !output.status.success() {
-            return Err(AppError::CmdFailed(
-                String::from_utf8_lossy(&output.stderr).to_string(),
-            ));
-        }
+        Command::new("sudo")
+            .current_dir("/home/scrom")
+            .arg(firecracker_path)
+            .arg("--api-sock")
+            .arg(API_SOCKET)
+            .arg("--enable-pci")
+            .spawn()
+            .map_err(|e| AppError::CmdFailed(e.to_string()))?;
 
         Ok(())
     }
