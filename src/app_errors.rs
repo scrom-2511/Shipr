4db0f5c04c406f0856b83a661288df7066ce6558
scrom-2511 <formain337@gmail.com>
2026-04-09 00:10:18 +0530

    update: app _error.rs file

--- src/app_errors.rs
+++ src/app_errors.rs
@@ -10,4 +10,22 @@ pub enum AppError {
 
     #[error("Queue declaration error: {0}")]
     QueueError(String),
+
+    #[error("Invalid git url")]
+    InvalidGitUrl,
+
+    #[error("Gitclone failed: {0}")]
+    GitCloneFailed(String),
+
+    #[error("Error creating dir: {0}")]
+    DirCreationFailed(String),
+
+    #[error("Running command failed: {0}")]
+    CmdFailed(String),
+
+    #[error("Failed getting the current working directory: {0}")]
+    CurrentWorkingDirUnavailable(String),
+
+    #[error("Failed to start firecracker: {0}")]
+    StartingFirecrackerFailed(String),
 }
