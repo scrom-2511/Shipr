46eda258c411e6b19017d1d1b04e1c97fbca1430
scrom-2511 <formain337@gmail.com>
2026-04-09 00:08:51 +0530

    add: start fn to start the firecracker

--- src/services/firecracker/firecracker.rs
+++ src/services/firecracker/firecracker.rs
@@ -10,4 +10,31 @@ impl Firecracker {
     pub fn new() -> Self {
         Self
     }
+
+    pub async fn start() -> Result<(), AppError> {
+        let API_SOCKET = "/tmp/firecracker.socket";
+
+        fs::remove_file(API_SOCKET);
+
+        let firecracker_run_cmd = format!(
+            "sudo ./firecracker-main --api-sock {} --enable-pci",
+            API_SOCKET
+        );
+
+        let firecracker_run_cmd_parts: Vec<&str> = firecracker_run_cmd.split_whitespace().collect();
+
+        let output = Command::new(&firecracker_run_cmd_parts[0])
+            .args(&firecracker_run_cmd_parts[1..])
+            .output()
+            .await
+            .map_err(|e| AppError::StartingFirecrackerFailed(e.to_string()))?;
+
+        if !output.status.success() {
+            return Err(AppError::CmdFailed(
+                String::from_utf8_lossy(&output.stderr).to_string(),
+            ));
+        }
+
+        Ok(())
+    }
 }
