ecadb5b0f9ddac66a7b859790c559383dfa29369
scrom-2511 <formain337@gmail.com>
2026-04-09 01:58:04 +0530

    add: configure_firecracker fn

--- src/services/firecracker/firecracker.rs
+++ src/services/firecracker/firecracker.rs
@@ -100,4 +100,85 @@ impl Firecracker {
 
         Ok(())
     }
+
+    async fn configure_firecracker() -> Result<(), AppError> {
+        let API_SOCKET = "/tmp/firecracker.socket";
+        let LOGFILE = "./firecracker.log";
+
+        let cmd_logger = format!(
+            r#"sudo curl -X PUT --unix-socket "{}" --data '{{ 
+                "log_path": "{}", 
+                "level": "Debug", 
+                "show_level": true, 
+                "show_log_origin": true 
+            }}' http://localhost/logger"#,
+            API_SOCKET, LOGFILE
+        );
+
+        let cmd_kernel = r#"KERNEL="./$(ls vmlinux* | tail -1)""#;
+        let cmd_boot_args = r#"KERNEL_BOOT_ARGS="console=ttyS0 reboot=k panic=1""#;
+        let cmd_arch = r#"ARCH=$(uname -m)"#;
+        let cmd_arch_cond = r#"if [ "$ARCH" = "aarch64" ]; then KERNEL_BOOT_ARGS="keep_bootcon ${KERNEL_BOOT_ARGS}"; fi"#;
+
+        let cmd_boot_source = format!(
+            r#"sudo curl -X PUT --unix-socket "{}" --data '{{ 
+                "kernel_image_path": '"$KERNEL"', 
+                "boot_args": '"$KERNEL_BOOT_ARGS"' 
+            }}' http://localhost/boot-source"#,
+            API_SOCKET
+        );
+
+        let cmd_rootfs = r#"ROOTFS="./$(ls *.ext4 | tail -1)""#;
+
+        let cmd_drive = format!(
+            r#"sudo curl -X PUT --unix-socket "{}" --data '{{ 
+                "drive_id": "rootfs", 
+                "path_on_host": '"$ROOTFS"', 
+                "is_root_device": true, 
+                "is_read_only": false 
+            }}' http://localhost/drives/rootfs"#,
+            API_SOCKET
+        );
+
+        let cmd_mac = r#"FC_MAC="06:00:AC:10:00:02""#;
+
+        let cmd_net = format!(
+            r#"sudo curl -X PUT --unix-socket "{}" --data '{{ 
+                "iface_id": "net1", 
+                "guest_mac": '"$FC_MAC"', 
+                "host_dev_name": "tap0" 
+            }}' http://localhost/network-interfaces/net1"#,
+            API_SOCKET
+        );
+
+        let cmd_sleep1 = "sleep 0.015";
+
+        let cmd_start = format!(
+            r#"sudo curl -X PUT --unix-socket "{}" --data '{{ 
+                "action_type": "InstanceStart" 
+            }}' http://localhost/actions"#,
+            API_SOCKET
+        );
+
+        let cmd_sleep2 = "sleep 2";
+
+        Self::run_cmds(&vec![
+            &cmd_logger,
+            cmd_kernel,
+            cmd_boot_args,
+            cmd_arch,
+            cmd_arch_cond,
+            &cmd_boot_source,
+            cmd_rootfs,
+            &cmd_drive,
+            cmd_mac,
+            &cmd_net,
+            cmd_sleep1,
+            &cmd_start,
+            cmd_sleep2,
+        ])
+        .await?;
+
+        Ok(())
+    }
 }
