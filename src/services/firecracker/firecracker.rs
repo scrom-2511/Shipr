a6c870801b6de31b5b62f32d4235c8e55f8bb5b9
scrom-2511 <formain337@gmail.com>
2026-04-09 01:44:51 +0530

    update_and_add: updated run_cmd fn and added set_network_interface fn

--- src/services/firecracker/firecracker.rs
+++ src/services/firecracker/firecracker.rs
@@ -9,23 +9,31 @@ impl Firecracker {
         Self
     }
 
-    async fn run_cmd(cmd: &str) -> Result<(), AppError> {
-        let cmd_parts: Vec<&str> = cmd.split_whitespace().collect();
+    async fn run_cmds(cmds: &Vec<&str>) -> Result<(), AppError> {
+        if cmds.len() == 0 {
+            return Ok(());
+        }
 
-        let initial_part = cmd_parts
-            .get(0)
-            .ok_or_else(|| AppError::CmdFailed("Empty command".into()))?;
+        for cmd in cmds {
+            let cmd_parts: Vec<&str> = cmd.split_whitespace().collect();
 
-        let output = Command::new(initial_part)
-            .args(&cmd_parts[1..])
-            .output()
-            .await
-            .map_err(|e| AppError::CmdFailed(e.to_string()))?;
+            print!("{:?}", cmd_parts);
 
-        if !output.status.success() {
-            return Err(AppError::CmdFailed(
-                String::from_utf8_lossy(&output.stderr).to_string(),
-            ));
+            let initial_part = cmd_parts
+                .get(0)
+                .ok_or_else(|| AppError::CmdFailed("Empty command".into()))?;
+
+            let output = Command::new(initial_part)
+                .args(&cmd_parts[1..])
+                .output()
+                .await
+                .map_err(|e| AppError::CmdFailed(e.to_string()))?;
+
+            if !output.status.success() {
+                return Err(AppError::CmdFailed(
+                    String::from_utf8_lossy(&output.stderr).to_string(),
+                ));
+            }
         }
 
         Ok(())
@@ -38,7 +46,7 @@ impl Firecracker {
 
         let firecracker_path = "/home/scrom/firecracker-main";
 
-        Self::run_cmds(&cleanup_cmd).await?;
+        Self::run_cmds(&vec![&cleanup_cmd]).await?;
 
         Command::new("sudo")
             .current_dir("/home/scrom")
@@ -48,6 +56,21 @@ impl Firecracker {
             .arg("--enable-pci")
             .spawn()
             .map_err(|e| AppError::CmdFailed(e.to_string()))?;
+
+        Ok(())
+    }
+
+    async fn set_network_interface() -> Result<(), AppError> {
+        let TAP_DEV = "tap0";
+        let TAP_IP = "172.16.0.1";
+        let MASK_SHORT = "/30";
+
+        let cmd_1 = format!("sudo ip link del {} 2> /dev/null || true", TAP_DEV);
+        let cmd_2 = format!("sudo ip tuntap add dev {} mode tap", TAP_DEV);
+        let cmd_3 = format!("sudo ip addr add {}{} dev {}", TAP_IP, MASK_SHORT, TAP_DEV);
+        let cmd_4 = format!("sudo ip link set dev {} up", TAP_DEV);
+
+        Self::run_cmds(&vec![&cmd_1, &cmd_2, &cmd_3, &cmd_4]).await?;
 
         Ok(())
     }
