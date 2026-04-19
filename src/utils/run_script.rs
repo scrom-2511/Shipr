ca68df5f7f99c299ea953bb53a4bca6e778d0c5a
scrom-2511 <formain337@gmail.com>
2026-04-19 12:30:34 +0530

    add: run_script fn

---
+++ src/utils/run_script.rs
@@ -0,0 +1,15 @@
+use crate::app_errors::AppError;
+use std::process::Command;
+
+pub fn run_script(script: Vec<&str>) -> Result<(), AppError> {
+    for cmd in script {
+        let output = Command::new("bash")
+            .arg("-c")
+            .arg(cmd)
+            .current_dir("/home/scrom")
+            .output()
+            .map_err(|e| AppError::StartingFirecrackerFailed(e.to_string()))?;
+    }
+
+    Ok(())
+}
