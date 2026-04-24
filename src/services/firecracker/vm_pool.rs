ec907846153f76dfce7c64212859d8085a71b96f
scrom-2511 <formain337@gmail.com>
2026-04-24 12:20:54 +0530

    add: ideal_pools for hot vms

--- src/services/firecracker/vm_pool.rs
+++ src/services/firecracker/vm_pool.rs
@@ -6,12 +6,14 @@ use uuid::Uuid;
 #[derive(Clone)]
 pub struct VmPool {
     pool: Arc<Mutex<HashMap<Uuid, u32>>>,
+    ideal_vms: Arc<Mutex<Vec<u32>>>,
 }
 
 impl VmPool {
     pub fn new() -> Self {
         Self {
             pool: Arc::new(Mutex::new(HashMap::new())),
+            ideal_vms: Arc::new(Mutex::new(Vec::new())),
         }
     }
 
@@ -23,5 +25,10 @@ impl VmPool {
     pub fn get_from_pool(&self, project_id: Uuid) -> Option<u32> {
         let pool = self.pool.lock().unwrap();
         pool.get(&project_id).copied()
+    }
+
+    pub fn add_to_ideal_vms(&self, vm_id: u32) {
+        let mut ideal_vms = self.ideal_vms.lock().unwrap();
+        ideal_vms.push(vm_id);
     }
 }
