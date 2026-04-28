c76f9ab461914f9f15814b63e3ad6dcf5b701a38
scrom-2511 <formain337@gmail.com>
2026-04-29 00:56:58 +0530

    update: run_script_vm fn added

--- src/utils/run_script.rs
+++ src/utils/run_script.rs
@@ -13,3 +13,16 @@ pub fn run_script(script: Vec<&str>) -> Result<(), AppError> {
 
     Ok(())
 }
+
+pub fn run_script_vm(script: Vec<&str>) -> Result<(), AppError> {
+    for cmd in script {
+        Command::new("bash")
+            .arg("-c")
+            .arg(cmd)
+            .current_dir("/root")
+            .output()
+            .map_err(|e| AppError::StartingFirecrackerFailed(e.to_string()))?;
+    }
+
+    Ok(())
+}
