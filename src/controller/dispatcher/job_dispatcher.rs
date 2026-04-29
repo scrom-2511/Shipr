cbbe8fe500db790eb7bf44a4a0291ccf25e2239f
scrom-2511 <formain337@gmail.com>
2026-04-29 18:20:51 +0530

    add: trait vmdetails, implemented it and updated get_orcreate_vm and move_json_to_vm fn

--- src/controller/dispatcher/job_dispatcher.rs
+++ src/controller/dispatcher/job_dispatcher.rs
@@ -1,21 +1,67 @@
+use core::fmt;
 use std::fs;
 use std::path::PathBuf;
 
 use url::Url;
+use uuid::Uuid;
 
 use crate::app_errors::AppError;
-use crate::app_types::{DeployDetails, ProjectType};
+use crate::app_types::{DeployDetails, JobType, RunDetails};
 use crate::controller::vm::firecracker::Firecracker;
-use crate::controller::vm::id_allocator::IdAllocator;
+use crate::controller::vm::vm_pool::VmPool;
 use crate::infra::process::run_script;
 
 pub struct JobDispatcher {
     vm: Option<Firecracker>,
+    vm_pool: VmPool,
+}
+
+impl fmt::Display for JobType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            JobType::Deploy => write!(f, "deploy"),
+            JobType::Run => write!(f, "run"),
+        }
+    }
+}
+
+pub trait VmDetails {
+    fn get_json(&self) -> String;
+    fn get_project_id(&self) -> Uuid;
+    fn get_job_type(&self) -> JobType;
+}
+
+impl VmDetails for DeployDetails {
+    fn get_json(&self) -> String {
+        serde_json::to_string(self).unwrap()
+    }
+
+    fn get_project_id(&self) -> Uuid {
+        self.project_id
+    }
+
+    fn get_job_type(&self) -> JobType {
+        JobType::Deploy
+    }
+}
+
+impl VmDetails for RunDetails {
+    fn get_json(&self) -> String {
+        serde_json::to_string(self).unwrap()
+    }
+
+    fn get_project_id(&self) -> Uuid {
+        self.project_id
+    }
+
+    fn get_job_type(&self) -> JobType {
+        JobType::Run
+    }
 }
 
 impl JobDispatcher {
-    pub fn new() -> Self {
-        Self { vm: None }
+    pub fn new(vm_pool: VmPool) -> Self {
+        Self { vm: None, vm_pool }
     }
 
     fn git_url_validator(&self, git_url: &str) -> Result<bool, AppError> {
@@ -28,24 +74,38 @@ impl JobDispatcher {
         Err(AppError::InvalidGitUrl)
     }
 
-    async fn create_vm(&mut self, id_allocator: IdAllocator) -> Result<(), AppError> {
-        let vm_id = id_allocator.allocate_id().await?;
-        let mut new_vm = Firecracker::new(vm_id as u32, ProjectType::Node);
-        new_vm.create_vm().await?;
-        self.vm = Some(new_vm);
+    async fn get_or_create_vm(&self, project_id: Uuid) -> Result<(u32, bool), AppError> {
+        match self.vm_pool.get_from_pool(project_id) {
+            Some(id) => {
+                println!("Using existing VM {}", id);
+                Ok((id, true))
+            }
+            None => {
+                let new_id = self
+                    .vm_pool
+                    .get_from_ideal_vms()
+                    .ok_or(AppError::NoAvailableVm)?;
 
-        Ok(())
+                println!("Starting VM {}", new_id);
+
+                Ok((new_id as u32, false))
+            }
+        }
     }
 
-    async fn move_json_to_vm(&self, deploy_details: &DeployDetails) -> Result<(), AppError> {
+    async fn move_json_to_vm(&self, vm_details: &impl VmDetails) -> Result<(), AppError> {
         let vm = self.vm.as_ref().unwrap();
         vm.get_base_id();
 
-        let job_json = serde_json::to_string(deploy_details)?;
+        let job_json = vm_details.get_json();
+
+        let project_id = vm_details.get_project_id();
+
+        let job_type = vm_details.get_job_type().to_string().to_lowercase();
 
         let job_json_path = format!(
-            "/home/scrom/shipr/job_jsons/{}.json",
-            deploy_details.project_id
+            "/home/scrom/shipr/job_jsons/{}/{}.json",
+            job_type, project_id
         );
 
         if let Some(parent) = PathBuf::from(&job_json_path).parent() {
@@ -65,18 +125,35 @@ impl JobDispatcher {
         Ok(())
     }
 
-    pub async fn dispatch_job(
+    pub async fn dispatch_clone_job(
         &mut self,
         deploy_details: &DeployDetails,
-        id_allocator: IdAllocator,
     ) -> Result<(), AppError> {
         self.git_url_validator(&deploy_details.url)?;
-        self.create_vm(id_allocator).await?;
+        self.get_or_create_vm(deploy_details.project_id).await?;
         self.move_json_to_vm(deploy_details).await?;
 
         run_script(vec![
             "scp -r -i /home/scrom/ubuntu.id_rsa /home/scrom/code/shipr/target/release/worker root@172.16.0.2:/root/worker",
         ])?;
+
+        self.vm
+            .as_ref()
+            .unwrap()
+            .execute_command("cd /root && ./worker job.json")?;
+
+        Ok(())
+    }
+
+    pub async fn dispatch_run_job(&mut self, run_details: &RunDetails) -> Result<(), AppError> {
+        let (vm_id, is_new) = self.get_or_create_vm(run_details.project_id).await?;
+        if is_new {
+            self.move_json_to_vm(run_details).await?;
+
+            run_script(vec![
+                "scp -r -i /home/scrom/ubuntu.id_rsa /home/scrom/code/shipr/target/release/worker root@172.16.0.2:/root/worker",
+            ])?;
+        }
 
         self.vm
             .as_ref()
