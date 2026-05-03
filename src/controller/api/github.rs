f361ce9105731138aca606754fe0c4d68aafecfc
scrom-2511 <formain337@gmail.com>
2026-05-03 17:31:39 +0530

    fix: get_installation_id and get_installation_access_token

--- src/controller/api/github.rs
+++ src/controller/api/github.rs
@@ -6,6 +6,7 @@ use std::{
 use jsonwebtoken::{Algorithm, EncodingKey, Header, crypto::CryptoProvider, encode};
 use reqwest::Client;
 use serde::Serialize;
+use serde_json::Value;
 
 use crate::app_errors::AppError;
 
@@ -48,11 +49,38 @@ impl Github {
         Ok(token)
     }
 
-    pub async fn get_installation_access_token(
-        &self,
-        installation_id: &u64,
-    ) -> Result<String, AppError> {
+    pub async fn get_installation_id(&self, owner: &str, repo: &str) -> Result<u32, AppError> {
         let jwt_token = self.generate_jwt()?;
+
+        println!("JWT Token: {}", jwt_token);
+
+        let url = format!(
+            "https://api.github.com/repos/{}/{}/installation",
+            owner, repo
+        );
+
+        let res = self
+            .client
+            .get(url)
+            .header("Authorization", format!("Bearer {}", jwt_token))
+            .header("User-Agent", "shipr-deployment")
+            .send()
+            .await
+            .unwrap();
+
+        let json = res.json::<serde_json::Value>().await.unwrap();
+
+        let installation_id = json["id"].as_u64().unwrap() as u32;
+
+        Ok(installation_id)
+    }
+
+    pub async fn get_installation_access_token(&self) -> Result<String, AppError> {
+        let jwt_token = self.generate_jwt()?;
+
+        let installation_id = self
+            .get_installation_id("scrom-2511", "shipr_test_project")
+            .await?;
 
         let url = format!(
             "https://api.github.com/app/installations/{}/access_tokens",
@@ -63,14 +91,36 @@ impl Github {
             .client
             .post(&url)
             .header("Authorization", format!("Bearer {}", jwt_token))
+            .header("User-Agent", "shipr-deployment")
             .send()
             .await
             .unwrap();
 
         let json = res.json::<serde_json::Value>().await.unwrap();
 
-        println!("{:?}", json);
+        let access_token = json["token"].as_str().unwrap();
 
-        Ok(String::from(""))
+        Ok(access_token.to_string())
+    }
+
+    pub async fn get_defualt_branch(&self, owner: &str, repo: &str) -> Result<String, AppError> {
+        let jwt_token = self.generate_jwt()?;
+
+        let url = format!("https://api.github.com/repos/{}/{}", owner, repo);
+
+        let res = self
+            .client
+            .get(url)
+            .header("Authorization", format!("Bearer {}", jwt_token))
+            .header("User-Agent", "shipr-deployment")
+            .send()
+            .await
+            .unwrap();
+
+        let json = res.json::<serde_json::Value>().await.unwrap();
+
+        let default_branch = json["default_branch"].as_str().unwrap();
+
+        Ok(default_branch.to_string())
     }
 }
