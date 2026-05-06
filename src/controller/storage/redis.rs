fe339d6e693452fe625bde24dcf30eff4fab235d
scrom-2511 <formain337@gmail.com>
2026-05-06 13:09:25 +0530

    add: init redis

---
+++ src/controller/storage/redis.rs
@@ -0,0 +1,36 @@
+use redis::{AsyncCommands, Client};
+
+use crate::app_errors::AppError;
+
+#[derive(Clone)]
+pub struct Redis {
+    client: Client,
+}
+
+impl Redis {
+    pub fn new() -> Self {
+        let client = Client::open("redis://default:gQAAAAAAAYZ4AAIgcDE1YmJjNmZmN2NlZjI0OTM0YmFmNmU3MjRkZGNjMDgzOA@steady-jackal-99960.upstash.io:6379").unwrap();
+
+        Self { client }
+    }
+
+    pub fn get_client(&self) -> Client {
+        self.client.clone()
+    }
+
+    pub async fn set(&self, key: &str, value: &str) -> Result<(), AppError> {
+        let mut conn = self.client.get_multiplexed_async_connection().await?;
+
+        let _: () = conn.set(key, value).await?;
+
+        Ok(())
+    }
+
+    pub async fn get(&self, key: &str) -> Result<String, AppError> {
+        let mut conn = self.client.get_multiplexed_async_connection().await?;
+
+        let value = conn.get(key).await?;
+
+        Ok(value)
+    }
+}
