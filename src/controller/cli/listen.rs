19f559db0e781f981bd0251250b92dba24a281c6
scrom-2511 <formain337@gmail.com>
2026-05-12 17:22:25 +0530

    modify: moved log handlers to logs.rs

--- src/controller/cli/listen.rs
+++ src/controller/cli/listen.rs
@@ -1,8 +1,8 @@
-use std::{collections::HashMap, fs, io::Write};
+use std::{collections::HashMap, fs};
 
 use actix_web::{
     App, HttpResponse, HttpServer,
-    web::{self, Bytes},
+    web::{self},
 };
 use serde_json::Value;
 use tokio::sync::{
@@ -25,7 +25,10 @@ use crate::{
             vm_pool::VmPool,
         },
     },
-    infra::kill_vm::kill_vm,
+    infra::{
+        kill_vm::kill_vm,
+        logs::{logs_handler, stream_logs_handler},
+    },
 };
 
 async fn redeploy_completed_handler(
@@ -169,90 +172,6 @@ async fn deploy_handler(
     Ok(HttpResponse::Ok().json(serde_json::json!({
         "project_id": project_id
     })))
-}
-
-async fn stream_logs_handler(
-    path: web::Path<String>,
-    logs_store: LogsStore,
-) -> Result<HttpResponse, AppError> {
-    let project_id = path.into_inner();
-
-    println!("Project ID: {}", project_id);
-
-    let sender = logs_store.lock().await.get(&project_id).cloned();
-
-    let sender = match sender {
-        Some(sender) => sender,
-        None => {
-            return Ok(HttpResponse::NotFound().finish());
-        }
-    };
-
-    println!("sender is {:?}", sender);
-
-    let mut rx = sender.subscribe();
-
-    let stream = async_stream::stream! {
-
-        let file_path = "/home/scrom/code/shipr/logs";
-
-        let old_logs = fs::read_to_string(format!("{}/{}.txt", file_path, project_id)).unwrap();
-
-        for line in old_logs.lines() {
-            yield Ok::<Bytes, actix_web::Error>(
-                Bytes::from(
-                    format!("data: {}\\n\\n", line)
-                )
-            );
-        }
-
-        loop {
-            match rx.recv().await {
-                Ok(msg) => {
-                    yield Ok::<Bytes, actix_web::Error>(
-                        Bytes::from(
-                            format!("data: {}\n\n", msg)
-                        )
-                    );
-                }
-
-                Err(_) => {
-                    break;
-                }
-            }
-        }
-    };
-
-    Ok(HttpResponse::Ok()
-        .insert_header(("Content-Type", "text/event-stream"))
-        .insert_header(("Cache-Control", "no-cache"))
-        .streaming(stream))
-}
-
-async fn logs_handler(logs_store: LogsStore, data: web::Bytes) -> Result<HttpResponse, AppError> {
-    let data = serde_json::from_slice::<Value>(&data).unwrap();
-
-    let project_id = data["project_id"].as_str().unwrap();
-
-    let log = data["log"].as_str().unwrap();
-
-    let file_path = "/home/scrom/code/shipr/logs";
-
-    let mut file = fs::OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open(format!("{}/{}.txt", file_path, project_id))
-        .unwrap();
-
-    writeln!(file, "{}", log).unwrap();
-
-    let tx = { logs_store.lock().await.get(project_id).cloned() };
-
-    if let Some(tx) = tx {
-        tx.send(log.to_string())?;
-    }
-
-    Ok(HttpResponse::Ok().finish())
 }
 
 pub async fn listen(
